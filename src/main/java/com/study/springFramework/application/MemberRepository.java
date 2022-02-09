5b8bec84c86ac9846ab73bfab72b45291e691918
mac_daehwan2yo <daehwan7989@naver.com>
2022-02-10 01:20:09 +0900

    test : MemberRepsotory  회원 조회 단위테스트 구성, failure case

--- src/main/java/com/study/springFramework/application/MemberRepository.java
+++ src/main/java/com/study/springFramework/application/MemberRepository.java
@@ -2,6 +2,10 @@ package com.study.springFramework.application;
 
 import com.study.springFramework.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository {
     Long save(Member member);
+
+    Optional<Member> findById(Long id);
 }
--- src/test/java/com/study/springFramework/application/MemberRepositoryTest.java
+++ src/test/java/com/study/springFramework/application/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package com.study.springFramework.application;
 
 import com.study.springFramework.domain.Grade;
 import com.study.springFramework.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,14 +11,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("회원 저장소 IO 에 대한 단위 테스트")
 public class MemberRepositoryTest {
     private MemberRepository memberRepository;
+    private Member member;
+
+    @BeforeEach
+    void init() {
+        member = new Member(1L, "홍길동", Grade.BASIC);
+    }
 
     @Test
     @DisplayName("회원을 저장한다.")
     void save() {
-        // given
-        Member member = new Member(1L, "홍길동", Grade.BASIC);
-
         // when/then
         assertThat(memberRepository.save(member)).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("회원을 Id 로 조회한다.")
+    void findById() {
+        // when/then
+        assertThat(memberRepository.findById(1L).orElseThrow(IllegalArgumentException::new));
     }
 }
